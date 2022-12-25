fd665f5368a35d499f2e360777b68fe00d0f293b
egrof <egrof@ETHANPC94>
2022-12-25 02:49:38 -0600

    Add project files.

---
+++ practice/Program.cs
@@ -0,0 +1,14 @@
+﻿String userInput = Console.ReadLine();
+
+if (userInput == "Computer what time is it?")
+{
+    DateTime currentTime = DateTime.Now;
+
+    string timeString = currentTime.ToString();
+
+    Console.WriteLine("The Current Time is: " + timeString);
+}
+ else
+    {
+            Console.WriteLine($" I am not programmed to answer that.");
+    }
